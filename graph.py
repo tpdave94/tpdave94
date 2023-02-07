64d5d6da1be52e68b7f624f8d08d30107d9026bb
tpdave94 <tpdave94@yahoo.com>
2023-02-07 15:15:19 +0530

    Stack implementation.

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 class Graph:
     """Graph Module."""
     def __init__(self, v):
-        self.v = 0
+        self.v = v
         self.e = 0
         self.adj = [[] for i in range(self.v)]
 
@@ -14,16 +14,16 @@ class Graph:
         visited[v] = True
 
         for i in self.adj[v]:
-            if not visited[v]:
+            if not visited[i]:
                 self.df_traversal(v, visited)
 
     def is_connected(self):
         visited = [False] * self.v
         self.df_traversal(0, visited)
-        return True if all(self.visited) else False
+        return True if all(visited) else False
 
     def isTree(self):
-        return self.is_connected() and self.e == self.v -1
+        return self.is_connected() and self.e == self.v - 1
 
 
 if __name__ == '__main__':
@@ -32,4 +32,5 @@ if __name__ == '__main__':
     g.add_edge(0, 2)
     g.add_edge(0, 3)
     g.add_edge(3, 4)
+    print("This is Tree." if g.isTree() else "This is Graph.")
 
